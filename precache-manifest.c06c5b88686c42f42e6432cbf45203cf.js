1fd8369fc0ca57b2b9a03ffcefa739eba64242e5
ayushakes <ayu.ayu.sharma@gmail.com>
2019-06-29 19:03:46 +0530

    Updates

---
+++ precache-manifest.c06c5b88686c42f42e6432cbf45203cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7836437701316ae2be148974a4b870ca",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "6984807337363b8b1599",
+    "url": "/my-react-app/static/css/main.8c3212db.chunk.css"
+  },
+  {
+    "revision": "942701b7cceb50f0cdc1",
+    "url": "/my-react-app/static/js/2.e9443209.chunk.js"
+  },
+  {
+    "revision": "6984807337363b8b1599",
+    "url": "/my-react-app/static/js/main.f617d0da.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5319b9f845383c9229813eeded5c0d53",
+    "url": "/my-react-app/static/media/mysvg.5319b9f8.svg"
+  },
+  {
+    "revision": "84ca10395a1b85f70d86cec5fd0bb724",
+    "url": "/my-react-app/static/media/profile-image.84ca1039.jpg"
+  }
+]);
